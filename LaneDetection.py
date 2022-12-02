ef40f48700c04e80083f13e3f75e77ee59c1ba06
sudharsan-007 <55453134+sudharsan-007@users.noreply.github.com>
2022-12-02 18:05:40 -0500

    Add files via upload

---
+++ LaneDetection.py
@@ -0,0 +1,88 @@
+import numpy as np
+import cv2
+import utlis
+
+####################################################
+
+cameraFeed= False
+videoPath = 'highway2.mp4'
+cameraNo= 1
+frameWidth= 640
+frameHeight = 480
+
+if cameraFeed:intialTracbarVals = [24,55,12,100] #  #wT,hT,wB,hB
+else:intialTracbarVals = [42,63,14,87]   #wT,hT,wB,hB
+
+####################################################
+
+
+if cameraFeed:
+    cap = cv2.VideoCapture("https://192.168.1.5:8080/video")
+    cap.set(3, frameWidth)
+    cap.set(4, frameHeight)
+else:
+    cap = cv2.VideoCapture(videoPath)
+count=0
+noOfArrayValues =10
+global arrayCurve, arrayCounter
+arrayCounter=0
+arrayCurve = np.zeros([noOfArrayValues])
+myVals=[]
+utlis.initializeTrackbars(intialTracbarVals)
+
+
+while True:
+
+    success, img = cap.read()
+    ##img = cv2.imread('test3.jpg')
+    if cameraFeed== False:
+        img = cv2.resize(img, (frameWidth, frameHeight))
+    #img = cv2.resize(img, (frameWidth, frameHeight)) ## since we r using ip adress camera
+    imgWarpPoints = img.copy()
+    imgFinal = img.copy()
+    imgCanny = img.copy()
+
+    imgUndis = utlis.undistort(img)
+    imgThres,imgCanny,imgColor = utlis.thresholding(imgUndis)
+    src = utlis.valTrackbars()
+    imgWarp = utlis.perspective_warp(imgThres, dst_size=(frameWidth, frameHeight), src=src)
+    imgWarpPoints = utlis.drawPoints(imgWarpPoints, src)
+    imgSliding, curves, lanes, ploty = utlis.sliding_window(imgWarp, draw_windows=True)
+
+    try:
+        curverad =utlis.get_curve(imgFinal, curves[0], curves[1])
+        lane_curve = np.mean([curverad[0], curverad[1]])
+        imgFinal = utlis.draw_lanes(img, curves[0], curves[1],frameWidth,frameHeight,src=src)
+
+        # ## Average
+        currentCurve = lane_curve // 50
+        if  int(np.sum(arrayCurve)) == 0:averageCurve = currentCurve
+        else:
+            averageCurve = np.sum(arrayCurve) // arrayCurve.shape[0]
+        if abs(averageCurve-currentCurve) >200: arrayCurve[arrayCounter] = averageCurve
+        else :arrayCurve[arrayCounter] = currentCurve
+        arrayCounter +=1
+        if arrayCounter >=noOfArrayValues : arrayCounter=0
+        cv2.putText(imgFinal, str(int(averageCurve)), (frameWidth//2-70, 70), cv2.FONT_HERSHEY_DUPLEX, 1.75, (0, 0, 255), 2, cv2.LINE_AA)
+
+    except:
+        lane_curve=00
+        pass
+
+    imgFinal= utlis.drawLines(imgFinal,lane_curve)
+    print(currentCurve)
+
+    imgThres = cv2.cvtColor(imgThres,cv2.COLOR_GRAY2BGR)
+    imgBlank = np.zeros_like(img)
+    imgStacked = utlis.stackImages(0.7, ([img,imgUndis,imgWarpPoints],
+                                         [imgColor, imgCanny, imgThres],
+                                         [imgWarp,imgSliding,imgFinal]
+                                         ))
+
+    cv2.imshow("PipeLine",imgStacked)
+    cv2.imshow("Result", imgFinal)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ readMe.md
@@ -0,0 +1,35 @@
+# Curved Lane Detection for Self Driving Cars | OpenCV Python
+
+
+[![Watch Video](https://github.com/murtazahassan/Curved-Lane-Detection/blob/master/CurvedLaneDetection%20(1).jpg)](https://youtu.be/-RuWTwDlot8)
+
+n this video we are going to learn how to detect curved lanes. We will detect not only the lanes but how much curvature is present in them so that we can use it in self driving applications.
+
+Subscribe its FREE !!! 😂😂😂
+
+#CurvedLaneDetection
+
+#SelfDrivingCar
+
+#OpenCv
+
+Product Links:
+
+Recommend Webcam for Computer Vision:
+https://amzn.to/2MNtVKZ
+
+Budget Webcam:
+https://amzn.to/2ZP47Ug
+
+Links:
+OpenCV Python Complete Course:
+https://www.youtube.com/watch?v=CJXIjApHYVs&list=PLMoSUbG1Q_r_sc0x7ndCsqdIkL7dwrmNF
+
+How to install Opencv in Python: https://youtu.be/CJXIjApHYVs
+
+Joining Images: https://youtu.be/Wv0PSs0dmVI
+
+Color Detection: https://youtu.be/Tj4zEX_pdUg
+
+Contours: https://youtu.be/Fchzk1lDt7Q
+
